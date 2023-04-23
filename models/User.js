5caf68bd1eb767f2a071a4638aff3c195117d0d2
Michael Welle <michaelpwelle@gmail.com>
2023-04-22 21:13:59 -0700

    models set up

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            max_length: 16,
+        },
+        email: {
+            type: String,
+            required: true,
+            max_length: 50,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],   
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
